56016874666244565129182883e7e27dce02e4c5
Josiah Campbell <campbjos@mail.gvsu.edu>
2014-03-21 01:50:01 -0400

    -  onClick for libButton (button from home that sends you to gallery app) is funtional ---no final point, it just sends you back

--- src/org/opencv/samples/imagemanipulations/homeActivity.java
+++ src/org/opencv/samples/imagemanipulations/homeActivity.java
@@ -5,13 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Josiah on 3/17/14.
  */
 public class homeActivity extends Activity implements View.OnClickListener {
 
-    Button camButton, galButton;
+    Button camButton, libButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,21 +20,32 @@ public class homeActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.home_item);
 
         camButton = (Button) findViewById(R.id.camera);
-        galButton = (Button) findViewById(R.id.galleryIn);
+        libButton = (Button) findViewById(R.id.galleryIn);
         camButton.setOnClickListener(this);
-        galButton.setOnClickListener(this);
+        libButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                Toast.makeText(this, "onActivityResult"
+                        + "\n\t\t\t\tcase 10", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onClick(View v) {
         if (v == camButton) {
             startActivity(new Intent(this, mainActivity.class));
-        if (v == galButton) {
-            Intent galIntent = new Intent();
-            galIntent.setType("image/*");
-            galIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(galIntent);
-            }
+        }
+        if (v == libButton) {
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+            final int ACTIVITY_SELECT_IMAGE = 10;
+            startActivityForResult(galleryIntent, ACTIVITY_SELECT_IMAGE);
         }
     }
 }
