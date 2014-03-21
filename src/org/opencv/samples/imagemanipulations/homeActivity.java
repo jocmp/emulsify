204802e43ac91a6ccb1bd312bd67959202fc5b3c
Josiah Campbell <campbjos@mail.gvsu.edu>
2014-03-20 23:22:34 -0400

    - combined the Reuben code with the Josiah code. hopefully it stays stable somehow.

--- src/org/opencv/samples/imagemanipulations/homeActivity.java
+++ src/org/opencv/samples/imagemanipulations/homeActivity.java
@@ -5,13 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Josiah on 3/17/14.
  */
 public class homeActivity extends Activity implements View.OnClickListener {
 
-    Button camButton;
+    Button camButton, galButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +20,18 @@ public class homeActivity extends Activity implements View.OnClickListener {
         setContentView(R.layout.home_item);
 
         camButton = (Button) findViewById(R.id.camera);
+        galButton = (Button) findViewById(R.id.galleryIn);
         camButton.setOnClickListener(this);
+        galButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         if (v == camButton) {
             startActivity(new Intent(this, mainActivity.class));
+        if (v == galButton) {
+            Toast.makeText(this, "It won't now, but it will soon!", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
